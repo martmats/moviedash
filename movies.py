0fb5538c76b6fd86eef0a22042fd764fb87d1c75
Marta <martamatiasroca@gmail.com>
2024-07-01 18:00:57 +0100

    Update movies.py

--- movies.py
+++ movies.py
@@ -62,7 +62,31 @@ local_css("style.css")
 
 # add logo add_logo("https://thesteptorial.com/wp-content/uploads/2024/07/logo-moviedash.png")
 # Sidebar Logo
-st.markdown("![](https://thesteptorial.com/wp-content/uploads/2024/07/logo-moviedash.png)", unsafe_allow_html=True)
+
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(https://thesteptorial.com/wp-content/uploads/2024/07/logo-moviedash.png/200/200);
+                background-repeat: no-repeat;
+                padding-top: 120px;
+                background-position: 20px 20px;
+            }
+            [data-testid="stSidebarNav"]::before {
+                content: "My Company Name";
+                margin-left: 20px;
+                margin-top: 20px;
+                font-size: 30px;
+                position: relative;
+                top: 100px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+
 
 # Sidebar Menu Initialisation
 st.sidebar.markdown('<h2>Discover Your Movie</h2>', unsafe_allow_html=True)
