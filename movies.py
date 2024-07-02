32f1249dbd13c348a6857e36d7132c19f90f53db
Marta <martamatiasroca@gmail.com>
2024-07-02 11:06:11 +0100

    Update movies.py

--- movies.py
+++ movies.py
@@ -342,6 +342,24 @@ if st.session_state.menu == "Trendy Films":
 
 
 #STREAMING OPTION SECTION------------------------------------------------------------------------------------------------------------------------
+# Define the mapping function for popularity
+def popularity_scale(vote_count):
+    # Set the thresholds based on the description provided
+    max_vote_count = int(movies_df['vote_count'].max())
+    min_vote_count = int(movies_df['vote_count'].min())
+    scale_6_threshold = 1600
+
+    # Calculate scale points for 1-10
+    if vote_count <= scale_6_threshold:
+        # Linear scale from min_vote_count to scale_6_threshold from 1 to 6
+        return np.interp(vote_count, [min_vote_count, scale_6_threshold], [1, 6])
+    else:
+        # Linear scale from scale_6_threshold to max_vote_count from 6 to 10
+        return np.interp(vote_count, [scale_6_threshold, max_vote_count], [6, 10])
+
+# Apply the function to your vote count data to find the min and max for the slider
+popularity_values = movies_df['vote_count'].apply(popularity_scale)
+min_popularity, max_popularity = int(popularity_values.min()), int(popularity_values.max())
 
 # "Streaming Options" section
 elif st.session_state.menu == "Streaming Options":
@@ -378,13 +396,12 @@ elif st.session_state.menu == "Streaming Options":
 
         popularity_range = st.slider(
             "📈 Select Popularity Range",
-            min_value=int(movies_df['vote_count'].min()),
-            max_value=int(movies_df['vote_count'].max()),
-            value=(int(movies_df['vote_count'].min()), int(movies_df['vote_count'].max())),
+            min_value=min_popularity,
+            max_value=max_popularity,
+            value=(min_popularity, max_popularity),
             format="%d",
-            help="Slide to choose between less popular to extremely popular movies based on vote count."
+            help="Slide to choose between less popular to extremely popular movies based on a 1 to 10 scale."
         )
-
     with col2:
         # Filter the dataframe based on the search query
         if search_query:
@@ -397,7 +414,7 @@ elif st.session_state.menu == "Streaming Options":
             filtered_movies_df['providers'].apply(lambda x: any(provider in x for provider in selected_providers)) &
             filtered_movies_df['genres'].apply(lambda x: any(genre in x for genre in selected_genres)) &
             filtered_movies_df['year'].between(year_filter[0], year_filter[1]) &
-            filtered_movies_df['vote_count'].between(popularity_range[0], popularity_range[1])
+            filtered_movies_df['vote_count'].apply(popularity_scale).between(popularity_range[0], popularity_range[1])
         ]
 
         # Display the filtered films in a grid layout
@@ -416,8 +433,8 @@ elif st.session_state.menu == "Streaming Options":
                         <div class="movie-info">
                             <h4>{film['title']}</h4>
                             <p><b>Provider:</b> {matching_provider}</p>
-                            <p><b>Release Date:</b> {format_release_date(film['release_date'].strftime('%d/%m/%Y'))}</p>
-                            <p class="rating">{format_rating(film['vote_average'])}</p>
+                            <p><b>Release Date:</b> {film['release_date'].strftime('%d/%m/%Y')}</p>
+                            <p class="rating">{film['vote_average']}</p>
                             <details>
                                 <summary>More info</summary>
                                 <p>{film['overview']}</p>
