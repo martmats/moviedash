13226dc01c4a88c9304d96e1aaadb15118d4da13
Marta <martamatiasroca@gmail.com>
2024-07-04 10:36:48 +0100

    Update movies.py

--- movies.py
+++ movies.py
@@ -262,53 +262,8 @@ if st.session_state.menu == "Trendy Films":
     st.markdown('</div>', unsafe_allow_html=True)
     
     
-# Weekly Hotpicks Section
+# Weekly or Monthly Hotpicks Section
 
-    st.markdown("""
-    <div style="background-color: #cacef4; padding: 20px; border-radius: 10px;">
-        <h2>This Week's Must-Watch Popcorn Flicks 🎥</h2>
-        <p>Catch the latest and greatest films hitting your screens this week!</p>
-        <div class="movies-container">
-    """, unsafe_allow_html=True)
-
-    # Fetch and display the trendy films for the week
-    trendy_films_week = get_trendy_films_week()
-    if trendy_films_week.empty:
-        st.write("No trendy films for this week.")
-    else:
-        # Sort films by vote count in descending order and select top 10
-        trendy_films_week = trendy_films_week.sort_values(by=['vote_count'], ascending=[False])
-        display_films_in_rows(trendy_films_week)
-
-    # Close the div
-    st.markdown("""
-        </div>
-    </div>
-    """, unsafe_allow_html=True)
-    
-# Monthly hoticks section get_trendy_films_month
-    st.markdown("""
-    <div style="background-color: #cacef4; padding: 20px; border-radius: 10px;">
-        <h2>This Month's Popcorn Picks: Must-Watch Movies 🍿</h2>
-        <p>Catch the latest and greatest films hitting your screens this month!</p>
-        <div class="movies-container">
-    """, unsafe_allow_html=True)
-
-    # Fetch and display the trendy films for the week
-    trendy_films_month = get_trendy_films_month()
-    if trendy_films_month.empty:
-        st.write("No trendy films for this week.")
-    else:
-        # Sort films by vote count in descending order and select top 10
-        trendy_films_month = trendy_films_month.sort_values(by=['vote_count'], ascending=[False])
-        display_films_in_rows(trendy_films_month)
-
-    # Close the div
-    st.markdown("""
-        </div>
-    </div>
-    """, unsafe_allow_html=True)
-    #--------------------------------------------------------------------------------------------------------------------------------
     st.markdown("""
     <div style="background-color: #cacef4; padding: 20px; border-radius: 10px;">
         <h2>Pick Your Popcorn Flicks 🎥</h2>
@@ -339,7 +294,6 @@ if st.session_state.menu == "Trendy Films":
         # Sort films by vote count in descending order and select top 10
         trendy_films = trendy_films.sort_values(by=['vote_count'], ascending=[False])
         display_films_in_rows(trendy_films)
-    #--------------------------------------------------------------------------------------------------------------------------------
     
 # Interesting Facts section
 
